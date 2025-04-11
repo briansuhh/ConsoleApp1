080b0a606c0c1fa69c82198d1251b4d1146dd57a
ayumi <brianmaysebastian@gmail.com>
2025-04-11 15:27:50 +0800

    created car questionnaires

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// File: CarApp.cs
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WelcomeMessage();
+        AskAboutCar();
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Car App!");
+    }
+
+    static void AskAboutCar()
+    {
+        Console.Write("What is your favorite car brand? ");
+        string carBrand = Console.ReadLine();
+
+        Console.Write("What model do you like the most? ");
+        string carModel = Console.ReadLine();
+
+        DisplayCarInfo(carBrand, carModel);
+    }
+
+    static void DisplayCarInfo(string brand, string model)
+    {
+        Console.WriteLine($"\nNice choice! You like the {brand} {model}.");
+        Console.WriteLine("Drive safe!");
+    }
+}
